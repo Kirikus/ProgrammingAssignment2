d656bec9a3cb2dc17e3b2030b3481172900a9ec9
Kirikus <kirikusan@gmail.com>
2015-03-15 18:36:05 +0300

    Completed assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a constructor for matrix class with cached inverse method.
+## cacheSolve is an API for getting the cached value of matrix inverse.
 
-## Write a short comment describing this function
+## Matrix class with cached inverse method
+## Methods:
+##  setMatrix(M) - sets the matrix as M
+##  getMatrix()  - retrives the previously set matrix
+##  inverse()    - calculate the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-# TEST
+  # Not sure that I could change variable name in this assigment.
+  cinverse <- matrix()
+  
+  setMatrix <- function(m) {
+    x <<- m
+    cinverse <<- matrix()
+  }
+  
+  getMatrix <- function() x
+  
+  inverse <- function(){
+    if(identical(cinverse, matrix()))
+    {
+      # Calculating matrix inverse...
+      cinverse <<- solve(x)
+      return(cinverse)
+    }
+    else
+    {
+      # Matrix inverse is cashed
+      return(cinverse)
+    }
+  }
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, inverse = inverse)
 }
 
 
-## Write a short comment describing this function
+## API for makeCacheMatrix$inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  x$inverse()
 }
